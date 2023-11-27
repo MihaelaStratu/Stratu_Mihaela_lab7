457e2f65e682bcaba8822bbbecad9daaa2b80ffb
MIHAELA STRATU <mihaela.stratu@stud.ubbcluj.ro>
2023-11-27 18:28:12 +0200

    laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,26 @@
-﻿namespace Stratu_Mihaela_lab7;
+﻿using System;
+using Stratu_Mihaela_lab7.Data;
+using System.IO;
+
+namespace Stratu_Mihaela_lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
